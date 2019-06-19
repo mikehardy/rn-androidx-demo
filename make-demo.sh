0c9f4b24d595897e806507c9b3c384b0547f6bae
Mike Hardy <github@mikehardy.net>
2019-06-19 04:59:04 -0500

    feat: added assembleRelease commands and react-native-razorpay
    this uncovers some latent resource errors (react-native-razorpay has one: https://github.com/razorpay/react-native-razorpay/pull/201)

--- make-demo.sh
+++ make-demo.sh
@@ -31,7 +31,7 @@ npm i react-native-gesture-handler
 react-native link react-native-gesture-handler
 npm i rn-fetch-blob
 react-native link rn-fetch-blob
-npm i "git+https://github.com/mikehardy/react-native-bottomsheet.git#androidx-dependency-fix"
+npm i react-native-bottomsheet
 react-native link react-native-bottomsheet
 npm i react-native-fbsdk
 react-native link react-native-fbsdk
@@ -43,6 +43,14 @@ sed -i -e $'s/new FBSDKPackage()/new FBSDKPackage(CallbackManager.Factory.create
 npm i react-native-maps
 react-native link react-native-maps
 
+# react-native-razorpay does not allow version overrides so compileSdk is 26 - that breaks.
+# I made an upstream PR to patch it so you can override, and default to 28 which works with AndroidX
+npm i "git+https://github.com/mikehardy/react-native-razorpay.git#allow-version-overrides"
+react-native link react-native-razorpay
+
+# Razorpay requires minSdk 19 - and this is so big now we need MultiDex if we don't go to 21
+# shouldn't affect AndroidX demonstration so we'll go with it
+sed -i -e $'s/minSdkVersion = 16/minSdkVersion = 21/' android/build.gradle
 
 # Set up AndroidX for RN0.59.9 which is still using support libraries
 echo "android.useAndroidX=true" >> android/gradle.properties
@@ -57,4 +65,11 @@ if [ "$(uname)" == "Darwin" ]; then # this works around env var problems in mac
   USER=`whoami`
   echo "sdk.dir=/Users/$USER/Library/Android/sdk" > android/local.properties
 fi
+
+# If you don't try assembleRelease you might miss some resource errors
+cd android/
+./gradlew assembleDebug
+./gradlew assembleRelease
+cd ..
+
 npx react-native run-android
